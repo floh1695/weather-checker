f3368364254faa3ec4233f32473b66d56bad0c2f
Bayley <cbayley@proforma.com>
2018-03-21 15:51:09 -0400

    All the way up to the api call

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,4 +1,51 @@
+const siteHead = 'http://api.openweathermap.org/data/2.5/weather?';
+const apiKey = 'f4d19a80c743b8354ac723b2953fa90b';
+const apiArg = () => {
+  return `&appid=${apiKey}`;
+};
+
+const appendToDataLog = (textContent) => {
+  const dataLog = document.querySelector('#dataLog');
+  const _p = document.createElement('p');
+  _p.textContent = textContent;
+  dataLog.appendChild(_p);
+};
+
+const checkWeather = () => {
+  const cityName = document.querySelector('#searchBar').value;
+  console.log('checking for city:', cityName);
+
+  const fetchMe = `${siteHead}q=${cityName}${apiArg()}`;
+  console.log('fetching:', fetchMe);
+
+  const handleResponse = (response) => {
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      console.warn('Response failed');
+    }
+  };
+
+  const handleData = (data) => {
+    console.log('data:', data);
+    appendToDataLog(data.name);
+  };
+
+  fetch(fetchMe).then(handleResponse).then(handleData);
+};
+
+const searchButtonConfig = () => {
+  const searchButton = document.querySelector('#searchButton');
+  const listener = (event) => {
+    event.preventDefault();
+    console.log('searchButton -> click listener');
+    checkWeather();
+  };
+  searchButton.addEventListener('click', listener);
+};
+
 const main = () => {
+  searchButtonConfig();
 };
 
 document.addEventListener('DOMContentLoaded', main);
